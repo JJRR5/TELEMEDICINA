378b8243552ba22f28ee3d6ae83550cf4dd0a0e3
Jose Rodriguez <76703666+JJRR5@users.noreply.github.com>
2022-02-17 14:51:26 -0600

    Practica Promedio DHT22 en App de Arduino

---
+++ promedioDHT22.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <BluetoothSerial.h>
+
+BluetoothSerial serialbt;
+
+int sensor = 26;
+float temp;
+float humedad;
+int finloop = 1;
+
+float promedioTemp;
+float promedioHumedad;
+
+DHT dht(sensor,DHT22);
+
+void promedio();
+
+void setup() {
+  Serial.begin(115000);
+  dht.begin();
+  serialbt.begin("PEPE CRACK");
+}
+
+void loop() {
+  temp = dht.readTemperature();
+  humedad = dht.readHumidity();
+  promedio();
+}
+
+void promedio(){
+  if (!(isnan(temp) && isnan(humedad))) {
+    promedioTemp += temp;
+    promedioHumedad += humedad;
+    finloop+=1;
+    if(finloop >= 60){
+      promedioTemp = promedioTemp / 60;
+      promedioHumedad = promedioHumedad / 60;
+      serialbt.println("Temperatura promedio: "+String(promedioTemp)+"ºC"+"\nHumedad promedio: "+String(promedioHumedad)+"%");
+      finloop = 1;
+    }
+  }else{
+    Serial.println("Error de medicion");
+  }
+  delay(1000);
+}
